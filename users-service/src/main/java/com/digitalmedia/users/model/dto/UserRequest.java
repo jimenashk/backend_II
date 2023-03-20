bb23270321ddcfc62aa4483d486e84db6b539fbc
jimenashk <jimenashk@gmail.com>
2023-03-20 13:07:59 -0300

    update

---
+++ users-service/src/main/java/com/digitalmedia/users/model/dto/UserRequest.java
@@ -0,0 +1,10 @@
+package com.digitalmedia.users.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class UserRequest {
+    @Schema(example = "avatar")
+    private String avatar;
+}
